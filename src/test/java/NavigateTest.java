1c4069376d71618bb9e16346d10193abb4e1b071
alexander <pancasancha4@gmail.com>
2020-05-27 16:16:16 +0300

    New class for manage web elements

---
+++ src/test/java/NavigateTest.java
@@ -0,0 +1,70 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class NavigateTest extends BaseSettingsTest {
+	private static WebElement element;
+	private static JavascriptExecutor executor;
+	protected String urlSBMainPage = "http://www.sberbank.ru/ru/person";
+	protected String insuranceMainPageXPath = "//span[text()='Страхование']";
+	protected String insuranceForTravellersXPath = "//a[@class='lg-menu__sub-link' and text()='Страхование путешественников']";
+
+	public NavigateTest() {
+		this.urlSBMainPage = urlSBMainPage;
+		this.insuranceMainPageXPath = insuranceMainPageXPath;
+		this.insuranceForTravellersXPath = insuranceForTravellersXPath;
+	}
+
+	protected void printTitleAndURL() {
+		System.out.println(getDriver().getTitle() + "\n" + getDriver().getCurrentUrl());
+	}
+
+	protected void goToPage(String page) {
+		getDriver().get(page);
+	}
+
+	protected void clickByXPath(String XPath) {
+		try {
+			getDriver().findElement(By.xpath(XPath)).click();
+		} catch (ElementClickInterceptedException e) {
+			clickByJS(XPath);
+		}
+	}
+
+	protected void clickByID(String ID) {
+		getDriver().findElement(By.id(ID)).click();
+	}
+
+	protected void waitUntilClickable(String XPath) {
+		WebElement element = getDriver().findElement(By.xpath(XPath));
+		getWait().until(ExpectedConditions.elementToBeClickable(element));
+	}
+
+	protected void waitUntilVisible(String XPath) {
+		getWait().until(ExpectedConditions.visibilityOfElementLocated(By.xpath(XPath)));
+	}
+
+	protected void waitUntilPresent(String XPath) {
+		getWait().until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPath)));
+	}
+
+	protected void clickByJS(String XPath) {
+		element = getDriver().findElement(By.xpath(XPath));
+		executor = (JavascriptExecutor) getDriver();
+		executor.executeScript("arguments[0].click();", element);
+	}
+
+	protected void goForward() {
+		getDriver().navigate().forward();
+	}
+
+	protected void goBack() {
+		getDriver().navigate().back();
+	}
+
+	protected void refreshPage() {
+		getDriver().navigate().refresh();
+	}
+}
